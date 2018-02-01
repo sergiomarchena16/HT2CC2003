63ea93be7b7a59667309933f8d96efc789d94563
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-01-31 21:09:34 -0600

    Clase principal y correcciones

--- CalculadoraPostfix/src/Calculos.java
+++ CalculadoraPostfix/src/Calculos.java
@@ -28,8 +28,7 @@ public class Calculos implements calculadora{
         for(int i=0; i<nospace.length; i++){
         data.add(nospace[i]);
         }
-        System.out.println(data);
-        for(int j=0; j<data.size(); j++)//recorre la expresion y agrega los numeros al stack hasta encontrarse un simbolo
+        for(int j=0; j<nospace.length; j++)//recorre la expresion y agrega los numeros al stack hasta encontrarse un simbolo
         {
             if(data.get(j).equals("+"))//Realiza la suma de los elementos del stack si encuentra un +
             {
@@ -38,8 +37,9 @@ public class Calculos implements calculadora{
                 for(int k=0; k<stack.size(); k++)
                 {
                  sumando += stack.peek();
-                 stack.pop();   
+                 stack.pop();
                 }
+                
                 stack.push(sumando);
             }
             else if(data.get(j).equals("*"))//Realiza la multiplicacion de los elementos del stack si encuentra un *
@@ -49,18 +49,18 @@ public class Calculos implements calculadora{
                 {
                     multiplicando=multiplicando*stack.peek();
                     stack.pop();
+                    
                 }
                 stack.push(multiplicando);
-                System.out.println(multiplicando);
             }
-            else if(data.get(j).equals("-"))//Realiza la resta de los elementos del stack si encuentra un *
+            else if(data.get(j).equals("-"))//Realiza la resta de los elementos del stack si encuentra un -
             {
                 double restando=0;
-                for(int k=0; k<stack.size(); k++)
-                {
-                 restando -= stack.peek();
-                 stack.pop();   
-                }
+                restando -= stack.peek();
+                stack.pop();
+                restando+= stack.peek();
+                stack.pop();
+                
                 stack.push(restando);
             }
             else if(data.get(j).equals("/"))//Realiza la division de los elementos del stack si encuentra un *
@@ -80,24 +80,19 @@ public class Calculos implements calculadora{
                         stack.pop();
                         dividiendo=dividiendo*stack.peek();
                         stack.pop();
-                        System.out.println(dividiendo);
                         return dividiendo;
-                        
                     }
                 }
                 stack.push(dividiendo);
             }
             else //si el indice del array no retorna uno de los valores, agrega un numero al stack
                 {
-                System.out.println(data.get(j) + "esta en else");
                 stack.push(Double.parseDouble((String) data.get(j)));
-                System.out.println(stack.peek() + "ultimo en stack");
                 }
         }
         double respuesta=stack.peek();
         stack.empty();
         data.clear();
-        System.out.println(respuesta);
          return respuesta;   //devuelve la respuesta
     }
     
--- CalculadoraPostfix/src/Principal.java
+++ CalculadoraPostfix/src/Principal.java
@@ -5,6 +5,7 @@
  */
 import java.util.*;
 import java.io.*;
+import static java.lang.Double.NaN;
 
 /**
  *
@@ -15,7 +16,37 @@ public class Principal {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO code application logic here
+        
+        String expresion="";
+        File datos = null;
+        FileReader reader = null;
+        BufferedReader buffer = null;
+        try {
+            datos = new File("datos.txt");
+            reader = new FileReader(datos);
+            buffer = new BufferedReader(reader);
+            calculadora postfixCalc = new Calculos();
+            String expresionnn;
+            while ((expresionnn = buffer.readLine()) != null) {
+                expresion = expresion + expresionnn;
+            }
+            if (Double.isNaN(postfixCalc.operar(expresion))) {
+                System.out.println("MATH ERROR");
+            } else {
+                System.out.println("Resultado: " + postfixCalc.operar(expresion));
+            }
+        } catch (Exception e) {
+            if(expresion.equals(""))
+            {
+            System.out.println("No se pudo hallar el archivo 'datos.txt'");
+            }
+            else{
+                System.out.println("El archivo 'datos.txt' contiene una letra o una expresion inoperable");
+            }
+            
+        }
 
     }
 
