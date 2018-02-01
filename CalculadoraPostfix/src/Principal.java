d9c8ec577344da939e856820b8df1e2c6b4b7439
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-01-31 19:23:06 -0600

    Finalizacion de clase calculos

---
+++ CalculadoraPostfix/src/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author cooli
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ CalculadoraPostfix/src/StackVector.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cooli
+ */
+import java.util.ArrayList;
+
+public class StackVector<E> implements Stack<E>
+{
+	protected ArrayList<E> data;
+
+	public StackVector()
+	// post: constructs a new, empty stack
+	{
+		data = new ArrayList<E>();
+	}
+
+	public void push(E item)
+	// post: the value is added to the stack
+	//          will be popped next if no intervening push
+	{
+		data.add(item);
+	}
+
+	public E pop()
+	// pre: stack is not empty
+	// post: most recently pushed item is removed and returned
+	{
+		return data.remove(size()-1);
+	}
+
+	public E peek()
+	// pre: stack is not empty
+	// post: top value (next to be popped) is returned
+	{
+		return data.get(size() - 1);
+	}
+	
+	public int size()
+	// post: returns the number of elements in the stack
+	{
+		return data.size();
+	}
+  
+	public boolean empty()
+	// post: returns true if and only if the stack is empty
+	{
+		return size() == 0;
+	}
+}
